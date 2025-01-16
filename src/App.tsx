b8a667232dd3e174a3d401148d14ff9ca847dd97
violaine <e.violaine@yahoo.fr>
2025-01-16 16:03:28 +0100

    set up shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <div className="text-3xl font-bold">
+      App
+      <Button>Click me</Button>
+      </div>
+  )
 }
 
 export default App;
