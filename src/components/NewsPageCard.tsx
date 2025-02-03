be13d9c8f4cddc869fc8f92e6c72cd1ac42bc538
violaine <e.violaine@yahoo.fr>
2025-02-03 19:19:07 +0100

    style card

--- src/components/NewsPageCard.tsx
+++ src/components/NewsPageCard.tsx
@@ -5,17 +5,19 @@ import { Link } from "react-router-dom";
 const NewsPageCard = ({news}: {news:News}) => {
     const {url, image_url, title, published_at, news_site, summary} = news;
   return (
-    <div className="">
+    <div className="grid grid-cols-1 lg:grid-cols-4 lg:gap-x-4">
+        <div className="p-2 overflow-hidden lg:col-span-1 h-[300px] md:h-[400px] lg:h-full">
         <Link to={url} target="_blank">
-            <img className="max-w-80" src={image_url} alt="main img" />
+            <img src={image_url} alt="main img" className="h-full w-full object-cover"/>
         </Link>
-        <div className="">
-            <p>{title}</p>
+        </div>
+        <div className="p-2 overflow-hidden lg:col-span-3 h-[300px] md:h-[200px] lg:h-full">
+            <p className="text-2xl font-bold">{title}</p>
             <p>{published_at.split("T")[0]}</p>
-            <p>
+            <p className="flex items-center">
                 <span>{news_site}</span>
                 <span>|</span>
-                <span>Read from source</span>
+                <span className="flex gap-x-2">Read from source</span>
                 <Link to={url} target="_blank">
                     <CircleArrowRight
                         color="var(--clr-violet)"
@@ -23,7 +25,7 @@ const NewsPageCard = ({news}: {news:News}) => {
                     />
                 </Link>
             </p>
-            <p>{summary}</p>
+            <p className="mt-4">{summary}</p>
         </div>
         <div className=""></div>
     </div>
