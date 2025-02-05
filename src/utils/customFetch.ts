ad1d2da2ee1444daac83151cdda40d8d047b138c
violaine <e.violaine@yahoo.fr>
2025-02-05 17:16:13 +0100

    creation types for hubbles + custom fetch

--- src/utils/customFetch.ts
+++ src/utils/customFetch.ts
@@ -1,7 +1,12 @@
 import axios from "axios"
 
-const snapiAPI = "https://api.spaceflightnewsapi.net/v4/articles/"
+const snapiAPI = "https://api.spaceflightnewsapi.net/v4/articles/";
+const datastroAPI = "https://www.datastro.eu/api/explore/v2.1/catalog/datasets/nasahubble/records";
 
 export const snapiCustomFetch = axios.create({
     baseURL: snapiAPI,
+});
+
+export const datastroCustomFetch = axios.create({
+    baseURL: datastroAPI,
 });
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -25,3 +25,34 @@ export type FiltersParams = {
 
 // super type formé à partir de newsresponse auquel on ajoute les params
 export type NewsResponseWithParams = { response : NewsResponse, params : FiltersParams}
+
+export type HubbleImage = {
+    photo_id: number;
+    photo_title:string;
+    photo_description : string;
+    photo_url_m:{
+        thumbnail: boolean;
+        filename: string;
+        format: string;
+        width: number;
+        mimetype: string;
+        id: string;
+        last_synchronized : string;
+        height: number;
+        url :string;
+    };
+    photo_date_taken: string;
+    photo_height: number;
+    photo_width: number;
+    photo_license : string;
+    album_id: number;
+    album_name_tags: string;
+    url: string;
+    tags: null;
+
+};
+
+export type HubbleImagesResponse = {
+    total_count : number;
+    results : HubbleImage[];
+}
