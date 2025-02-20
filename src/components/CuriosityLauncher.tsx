1e29ce0aa2e71485ffcaacfbe9b5aa1ec953fa92
violaine <e.violaine@yahoo.fr>
2025-02-20 18:27:35 +0100

    launchers curiosity + webb components and style for landing pages

--- src/components/CuriosityLauncher.tsx
+++ src/components/CuriosityLauncher.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 function CuriosityLauncher() {
   return (
     <article className="test w-full h-[40vh] curiosity">
-        <div className="align-element text-white p-2 mt-20">
+        <div className="align-element text-white p-2 mt-20 ">
             <p className="font-bold text-4xl capitalize"> More pictures of Curiosity on Mars</p>
-            <button type="button" className="mt-4">
+            <button type="button" className="mt-4 slider-button">
                 <Link to="/nasa">Explore</Link>
             </button>
         </div>
--- src/components/WebbLauncher.tsx
+++ src/components/WebbLauncher.tsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom"
 
 
 function WebbLauncher() {
   return (
-    <div>WebbLauncher</div>
+   <article className="test w-full  h-[40vh] webb pt-24">
+    <div className="align-element text-white p-2 flex flex-col">
+      <p className="font-bold text-4xl capitalize max-w-[40%] place-self-end">
+        The most recent photos taken by the James Webb Telescope</p>
+      <button type="button" className="slider-button mt-4 place-self-end text-right">
+        <Link to="/webb">Explore</Link>
+      </button>
+    </div>
+   </article>
   )
 }
 
