ff9e00db0fbddd2c34387d739d78d843c37bd593
violaine <e.violaine@yahoo.fr>
2025-02-24 19:16:32 +0100

    fix homepage style

--- src/components/CuriosityLauncher.tsx
+++ src/components/CuriosityLauncher.tsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom"
 
 function CuriosityLauncher() {
   return (
-    <article className="w-full h-[40vh] curiosity">
-        <div className="align-element text-white p-2 mt-20 ">
-            <p className="font-bold text-4xl capitalize"> Last pictures of Curiosity on Mars</p>
-            <button type="button" className="mt-4 slider-button">
+    <article className="w-full h-[50vh] curiosity pt-24">
+        <div className="align-element text-white p-2 flex flex-col">
+            <p className="font-bold text-4xl capitalize max-w-[40%] place-self-start"> Last pictures of Curiosity on Mars</p>
+            <button type="button" className="slider-button mt-4 place-self-start text-left">
                 <Link to="/nasa">Explore</Link>
             </button>
         </div>
