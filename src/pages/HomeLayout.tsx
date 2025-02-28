a1677fd173638a750a7ab2757def29a1f82cbeef
violaine <e.violaine@yahoo.fr>
2025-02-28 14:26:41 +0100

    scroll to top when user changes pages

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,7 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header, Footer, Navbar } from "@/components";
+import { useEffect } from "react";
 
 const HomeLayout = () => {
+ const location = useLocation();
+ 
+// scroll to top when change page 
+ useEffect(() => {
+    window.scrollTo(0, 0);
+ }, [location])
+
     return <>
         <Header/>
         <Navbar/>
